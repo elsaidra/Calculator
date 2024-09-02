3d594ce6d0d9f882872786b2075f1042a2df62a6
Elsaid Wael <alghnam906@gmail.com>
2024-09-02 19:13:56 +0300

    userIn.textContent += event.target.value; add the things that poppulate the display and such

--- script/script.js
+++ script/script.js
@@ -10,3 +10,35 @@ function operate (n1, ope, n2) {
     else if ( ope === '*' ) {return multiply(n1, n2)}
     else if ( ope === '/' ) {return divide(n1, n2)}
 }
+let display = document.querySelector('.display')
+let userIn = document.querySelector('.user-input')
+let result = document.querySelector('.result')
+let numbersBu = document.querySelectorAll('.numbers')
+let operator = document.querySelectorAll('.operator')
+let clear = document.querySelectorAll('.clear')
+
+let currentOperator = null;
+let firstOperand = null;
+let secondOperand = null;
+
+//event listeners for the numbers
+numbersBu.forEach(btn => {
+    btn.addEventListener('click', (event) => {
+    if (event.target.value === '.') {
+        if (!userIn.textContent.includes('.')) {
+            userIn.textContent += event.target.value; 
+        }
+        else return;
+    }
+
+    if (event.target.value !== '.')  userIn.textContent += event.target.value; 
+    })
+});
+
+// clear 
+
+clear.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        userIn.textContent = " ";
+    })
+})
